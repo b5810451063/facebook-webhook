3151a3d6b2fa983c944c41145da69ed6055242d4
jay.sakchai <sakchai.limtsooknirun@scb.co.th>
2020-01-10 15:21:08 +0700

    update

--- app.js
+++ app.js
@@ -815,7 +815,7 @@ function callSendAPI(messageData) {
     json: messageData
 
   }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
 
